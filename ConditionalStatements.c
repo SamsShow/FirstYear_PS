7e62bc4766767c06e9e6ad323e53380b57b4021e
Saksham Tyagi <74039834+SamsShow@users.noreply.github.com>
2023-01-29 20:19:05 +0530

    Create ConditionalStatements.c

---
+++ ConditionalStatements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    
+    if(a == b && b == c)
+        printf("Equilateral");
+    else if(a != b && b != c && a!=c)
+        printf("Scalene");
+    else
+        printf("Isosceles");
+    
+    
+    return 0;
+}
