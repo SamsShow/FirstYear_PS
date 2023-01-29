f3ea88678abe7ae8e71c5ba56199ef121ee0117f
Saksham Tyagi <74039834+SamsShow@users.noreply.github.com>
2023-01-29 22:10:29 +0530

    Update SmartNumber.c

--- SmartNumber.c
+++ SmartNumber.c
@@ -38,3 +38,9 @@ int main() {
         printf("No\n");   
     return 0;
 }
+
+//This program takes an integer num as input and checks if it is a smart number or not. 
+//The countFactors function calculates the number of factors of num. 
+//If the number of factors is odd, isSmartNumber returns 1 which represents that num is a smart number. 
+//If the number of factors is even, isSmartNumber returns 0 which represents that num is not a smart number. 
+//Finally, the result of isSmartNumber is printed as "Yes" if num is a smart number and "No" if it is not.
