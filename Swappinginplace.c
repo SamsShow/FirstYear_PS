56b5208465abb0a24b403b4f8ef027f0038930fd
Saksham Tyagi <74039834+SamsShow@users.noreply.github.com>
2023-01-29 22:13:29 +0530

    Update Swappinginplace.c

--- Swappinginplace.c
+++ Swappinginplace.c
@@ -1,9 +1,9 @@
 int main() {
-int a,b;
-scanf("%d %d", &a,&b);
-a=a+b;
-b=a-b;
-a=a-b;
-printf("%d %d",a,b);
+    int a,b;
+    scanf("%d %d", &a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d %d",a,b);
     return 0;
 }
